4469e4d5c5cf0c7ad3b0ccebc6ca49fe57943242
jmjles <jmjles@gmail.com>
2019-11-11 22:35:16 -0800

    stretch complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
-
+import {Route, Switch } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import {Container, Paper} from '@material-ui/core';
 
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
 
@@ -12,7 +15,12 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      <Container>
+        <Paper>
+            <Route exact path='/movies/:id' render={() => <Movie addToSavedList= {addToSavedList}/>}/>
+            <Route exact path='/' render={() => <MovieList/>}/>
+        </Paper>
+      </Container>
     </div>
   );
 };
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import {Link,NavLink , Switch} from 'react-router-dom'
 const SavedList = props => (
+  
   <div className="saved-list">
+    {console.log(props)}
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
-      <span className="saved-movie">{movie.title}</span>
+        <NavLink className="saved-movie" activeClassName='active' to={`/movies/${movie.id}`} key={movie.title} >{movie.title}</NavLink>
     ))}
-    <div className="home-button">Home</div>
+    <Link className="home-button" to='/'>Home</Link>
   </div>
 );
 
